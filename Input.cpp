50f0075503b222927b24182569da32ec23619673
GauvainWU <gauvain.wujiahua@gmail.com>
2018-12-04 22:57:09 +0100

    Add input module.

---
+++ Input.cpp
@@ -0,0 +1,119 @@
+//
+// Created by Jiahua WU on 04.12.18.
+//
+
+// STL include
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <cctype>
+#include <algorithm>
+
+// Local include
+#include "Input.h"
+using namespace std;
+
+// Constructors
+Reader::Reader(const string& path): path(path){}
+Data_Reader::Data_Reader(const string& path, char delimiter): Reader(path),delimiter(delimiter){}
+Vector_Reader::Vector_Reader(const string& path, char delimiter): Data_Reader(path, delimiter){}
+Matrix_Reader::Matrix_Reader(const string &path, char delimiter): Data_Reader(path, delimiter){}
+Setting_Reader::Setting_Reader(const string &path): Reader(path){}
+
+// Destructors
+Vector_Reader::~Vector_Reader() {y.resize(0);}
+Matrix_Reader::~Matrix_Reader() {for (auto& vector: A) vector.resize(0); A.resize(0);}
+
+// Accessors
+Vector Vector_Reader::access_vector() const {return y;}
+Matrix Matrix_Reader::access_matrix() const {return A;}
+Setting Setting_Reader::access_setting() const {return setting;}
+
+//Read in functions
+void Vector_Reader::read_in() {
+    ifstream read_file(path);
+    while (!read_file.eof()) {
+        string s;
+
+        // Read the first line in s and end the loop if the file is empty
+        if (!getline(read_file, s)) break;
+
+        // Initialize an istream with s(first line of the document)
+        istringstream ss(s);
+        
+        // Parse through ss(each loop corresponds to each line)
+        while (ss) {
+            string s;
+            if (!getline(ss, s, delimiter)) break;
+            y.push_back(stod(s));
+        }
+    }
+    read_file.close();
+}
+
+void Matrix_Reader::read_in() {
+    ifstream read_file(path);
+
+    while (!read_file.eof()) {
+        string s;
+
+        // End the loop if the file is empty
+        if (!getline(read_file, s)) break;
+
+        istringstream ss(s);
+        vector<double> record;
+        while (ss) {
+            string s;
+            if (!getline(ss, s, delimiter)) break;
+            record.push_back(stod(s));
+        }
+        A.push_back(record);
+    }
+    read_file.close();
+}
+
+void Setting_Reader::read_in() {
+    ifstream read_file(path);
+    string value;
+    string parameter;
+    string tmp;
+    while (!read_file.eof()) {
+        string s;
+
+        // Read the first line in s and end the loop if the file is empty
+        if (!getline(read_file, s)) break;
+
+        // Initialize an istream with s(first line of the document)
+        istringstream ss(s);
+
+        // Parse through ss(each loop corresponds to each line)
+        while (ss) {
+            getline(ss, parameter, ' ');
+            getline(ss, tmp, '=');
+            getline(ss, value, '#');
+            break;
+        }
+
+        // Remove space in the string
+        value.erase(remove(value.begin(), value.end(), ' '), value.end());
+        parameter.erase(remove(parameter.begin(), parameter.end(), ' '), parameter.end());
+
+        if (parameter == "t0") setting.t0 = stod(value);
+        else if (parameter == "tn") setting.tn = stod(value);
+        else if (parameter == "path_y00") setting.path_y00 = value;
+        else if (parameter == "path_A") setting.path_A = value;
+        else if (parameter == "function_type") setting.function_type = value;
+        else if (parameter == "number_of_steps") setting.M = stoi(value);
+        else if (parameter == "name_of_solution") setting.name_of_solution = value;
+        else if (parameter == "precision") setting.precision = stoi(value);
+        else if (parameter == "delimiter") {
+            char char_array[2];
+            strcpy(char_array,value.c_str());
+            setting.delimiter = char_array[0];
+        }
+        else cerr << "unvalid input";
+    }
+    read_file.close();
+}
---
+++ Input.h
@@ -0,0 +1,83 @@
+//
+// Created by Jiahua WU on 04.12.18.
+//
+
+#ifndef ODE_SOLVER_INPUT_H
+#define ODE_SOLVER_INPUT_H
+
+#include "ODE_solver.h"
+#include <string>
+using namespace std;
+
+struct Setting{
+    int M;
+    int precision = 5;
+    Real t0;
+    Real tn;
+    string path_y00;
+    string path_A;
+    string name_of_solution;
+    string function_type;
+    char delimiter = ',';
+};
+
+class Reader{
+protected:
+    string path;
+
+public:
+    explicit Reader(const string& path);
+    virtual void read_in() = 0;
+};
+
+class Data_Reader : public Reader{
+protected:
+    char delimiter;
+
+public:
+    Data_Reader(const string& path,char delimiter);
+    virtual ~Data_Reader() = default;
+};
+
+class Vector_Reader : public Data_Reader{
+protected:
+    Vector y;
+
+public:
+    Vector_Reader(const string& path,char delimiter = ',');
+    ~Vector_Reader() override;
+
+    Vector access_vector() const;
+
+    void read_in() override;
+
+};
+
+class Matrix_Reader : public Data_Reader{
+protected:
+    Matrix A;
+
+public:
+    Matrix_Reader(const string& path, char delimiter = ',');
+    ~Matrix_Reader() override;
+
+    Matrix access_matrix() const;
+
+    void read_in() override;
+
+};
+
+class Setting_Reader : public Reader{
+protected:
+    Setting setting;
+
+public:
+    explicit Setting_Reader(const string& path);
+
+    Setting access_setting() const;
+    void read_in() override;
+
+};
+
+
+#endif //ODE_SOLVER_INPUT_H
