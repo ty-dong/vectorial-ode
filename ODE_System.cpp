b72254f46fd908e59cea144e651a9a9377463626
GauvainWU <gauvain.wujiahua@gmail.com>
2018-12-04 22:58:21 +0100

    Update constructor and output message

--- ODE_System.cpp
+++ ODE_System.cpp
@@ -13,17 +13,20 @@
 #include "ODE_System.h"
 using namespace std;
 
-ODE_System::ODE_System(double t0, double tn,const Vector& y00, const Matrix& A, Vector g(Real)):
-        A(A), t0(t0),tn(tn),y00(y00),solution(1,y00){
+ODE_System::ODE_System(Real t0, Real tn,const Vector& y00, const Matrix& A, Vector g(Real),int M):
+        A(A), t0(t0),tn(tn),y00(y00),solution(1,y00),M(M){
     // Check the match of dimension
     if(A[0].size() != y00.size()) {
-        cout << "Warning: Mismatch of dimension between Matrix A and vector variable." << endl;
+        cerr << "Error: Mismatch of dimension between Matrix A and vector variable.\n" << endl;
     }
     // Initialize function pointer
     ODE_System::g = g;
 }
 
 ODE_System::~ODE_System() {
+    for (auto& vector : A){
+        vector.resize(0);
+    }
     A.resize(0);
     solution.resize(0);
     y00.resize(0);
@@ -31,29 +34,26 @@ ODE_System::~ODE_System() {
 
 Matrix ODE_System::access_solution() const { return solution;}
 
-void ODE_System::set_A(const Matrix &other) { A = other; }
-void ODE_System::set_g(Vector other(Real)) { g = other; }
-void ODE_System::set_y0(const Vector & other) { y00 = other;}
-
-void ODE_System::write_solution(string filename, int precision) {
+void ODE_System::write_solution(string filename, int precision, char delimiter) {
     ofstream outFile;
     outFile.open(filename + ".dat",ios::out);
     // Check if the file is open as expected
-    if (!outFile.is_open()){cout << "File open failure. Output is unsuccessful"; return;};
+    if (!outFile.is_open()){cerr << "Error: File open failure. Output is unsuccessful.\n"; return;};
 
     for (auto vector :solution){
         for (auto value : vector){
             outFile.precision(precision);
-            outFile << value << ',';
+            outFile << value << delimiter;
         }
         outFile << endl;
     }
+    cout << "Solution is successfully written." << endl ;
     outFile.close();
 }
 
-ForwardEuler_System::ForwardEuler_System(double t0, double tn,
-                                         const Vector& y00, const Matrix& A, Vector g(Real)):
-        ODE_System(t0, tn, y00, A, g){}
+ForwardEuler_System::ForwardEuler_System(Real t0, Real tn,
+                                         const Vector& y00, const Matrix& A, Vector g(Real), int M):
+        ODE_System(t0, tn, y00, A, g, M){}
 
 ForwardEuler_System::~ForwardEuler_System() {
     A.resize(0);
@@ -61,7 +61,7 @@ ForwardEuler_System::~ForwardEuler_System() {
     y00.resize(0);
 }
 
-void ForwardEuler_System::solve(int M) {
+void ForwardEuler_System::solve() {
     try {
         solution = ForwardEuler(t0,tn,y00,M,A,g);
     } catch (timesteppossitive& e1){
@@ -71,9 +71,9 @@ void ForwardEuler_System::solve(int M) {
     }
 }
 
-Adams_Bashforth_System::Adams_Bashforth_System(double t0, double tn,
-                                               const Vector& y00,const Matrix& A, Vector g(Real), int step):
-        ODE_System(t0, tn, y00, A, g),step(step){}
+Adams_Bashforth_System::Adams_Bashforth_System(Real t0, Real tn,
+                                               const Vector& y00,const Matrix& A, Vector g(Real), int step, int M):
+        ODE_System(t0, tn, y00, A, g,M),step(step){}
 
 Adams_Bashforth_System::~Adams_Bashforth_System() {
     A.resize(0);
@@ -81,7 +81,7 @@ Adams_Bashforth_System::~Adams_Bashforth_System() {
     y00.resize(0);
 }
 
-void Adams_Bashforth_System::solve(int M) {
+void Adams_Bashforth_System::solve() {
     try {
         solution = Adams_Bashforth(t0, tn, y00, M, step, A, g);
     } catch (timesteppossitive& e1){
@@ -93,9 +93,9 @@ void Adams_Bashforth_System::solve(int M) {
     }
 }
 
-RKSystem4th_System::RKSystem4th_System(double t0, double tn,
-                                       const Vector& y00, const Matrix& A, Vector g(Real)):
-        ODE_System(t0, tn, y00, A, g){}
+RKSystem4th_System::RKSystem4th_System(Real t0, Real tn,
+                                       const Vector& y00, const Matrix& A, Vector g(Real),int M):
+        ODE_System(t0, tn, y00, A, g, M){}
 
 RKSystem4th_System::~RKSystem4th_System() {
     A.resize(0);
@@ -103,7 +103,7 @@ RKSystem4th_System::~RKSystem4th_System() {
     y00.resize(0);
 }
 
-void RKSystem4th_System::solve(int M) {
+void RKSystem4th_System::solve() {
     try {
         solution = RKSystem4th(t0, tn, y00, M, A, g);
     } catch (timesteppossitive& e1){
--- ODE_System.h
+++ ODE_System.h
@@ -26,38 +26,35 @@ public:
     typedef Vector(*Funpointer)(Real);
 
     /** \brief Constructor. Initializes ODE_System data structures*/
-    ODE_System(double t0, double tn, const Vector& y00, const Matrix& A, Vector g(Real));
+    ODE_System(Real t0, Real tn, const Vector& y00, const Matrix& A, Vector g(Real), int M);
     /** \brief Destructor. Deallocate memory used by std::vector objects*/
     virtual ~ODE_System();
 
     /** \brief Accessor to get the solution*/
     Matrix access_solution() const;
 
-    /** \brief Set the Matrix A*/
-    void set_A(const Matrix& A);
-    /** \brief Set the function g*/
-    void set_g(Vector g(Real));
-    /** \brief Set initial condition */
-    void set_y0(const Vector& y0);
-
     /**
      * \brief Pure virtual method. Call the solvers to solve the system.
      * @param M: number of time steps
      */
-    virtual void solve(int M) = 0;
+    virtual void solve() = 0;
 
     /**
      * \brief Output the solution as filetype given by the user\n
      * @param precision: number of significant numbers\n
-     * @param filename: name of the file in form of a string
+     * @param filename: name of the file in form of a string\n
+     * @param delimiter: character that seperates values
      */
-    void write_solution(string filename,int precision = 5);
+    void write_solution(string filename,int precision = 5, char delimiter = ',');
 
 protected:
     /** \brief Initial time*/
     Real t0;
     /** \brief End time*/
     Real tn;
+
+    /** \brief number of steps*/
+    int M;
 
     /** \brief nitial condition*/
     Vector y00;
@@ -78,7 +75,7 @@ protected:
 class ForwardEuler_System : public ODE_System{
 public:
     /** \brief Constructor. Initialize data sturcture. */
-    ForwardEuler_System(double t0, double tn, const Vector& y00, const Matrix& A, Vector g(Real));
+    ForwardEuler_System(Real t0, Real tn, const Vector& y00, const Matrix& A, Vector g(Real),int M);
 
     /** \brief Destructor. Deallocate memory used by std::vector objects*/
     ~ForwardEuler_System () override;
@@ -86,16 +83,16 @@ public:
     /** \brief Implement explicit forward euler method and store the solution at all time steps in the attribute solution */
     /** f(t,y)=A*y+g(t)\n
      * y_{n+1}=y_n+f(t_n,y_n)*h \n
-     * @param M number of time steps
+     * @param M number of steps
      */
-    void solve(int M)  override;
+    void solve() override;
 };
 
 /// Inheritance of class ODE_System: Solving the vectorial system by means of Adams Bashforth method
 class Adams_Bashforth_System : public ODE_System{
 public:
     /** \brief Constructor. Initialize data sturcture. */
-    Adams_Bashforth_System(double t0, double tn, const Vector& y00, const Matrix& A, Vector g(Real),int step);
+    Adams_Bashforth_System(Real t0, Real tn, const Vector& y00, const Matrix& A, Vector g(Real),int step,int M);
 
     /** \brief Destructor. Deallocate memory used by std::vector objects*/
     ~Adams_Bashforth_System() override;
@@ -106,9 +103,9 @@ public:
      * step=2: y_{n+2}=y_{n+1}+h*(3/2*f(t_{n+1},y_{n+1})-1/2*f(t_n,y_n)) \n
      * step=3: y_{n+3}=y_{n+2}+h*(23/12*f(t_{n+2},y_{n+2})-16/12*f(t_{n+1},y_{n+1})+5/12*f(t_n,y_n))\n
      * step=4: y_{n+4}=y_{n+3}+h*(55/24*f(t_{n+3},y_{n+3})-59/24*f(t_{n+2},y_{n+2})+37/24*f(t_{n+1},y_{n+1})-9/24*f(t_n,y_n)) \n
-     *  @param M number of time steps
+     *  @param M number of steps
      */
-    void solve(int M) override;
+    void solve() override;
 
 
 protected:
@@ -120,7 +117,7 @@ protected:
 class RKSystem4th_System : public ODE_System{
 public:
     /** \brief Constructor. Initialize data sturcture.*/
-    RKSystem4th_System(double t0, double tn, const Vector& y00,const Matrix& A, Vector g(Real));
+    RKSystem4th_System(Real t0, Real tn, const Vector& y00,const Matrix& A, Vector g(Real),int M);
 
     /** \brief Destructor. Deallocate memory used by std::vector objects*/
     ~RKSystem4th_System() override;
@@ -132,9 +129,9 @@ public:
      * k_3=f(t_n+0.5*h,y_n+0.5*k_2)) \n
      * k_4=f(t_n+h,y_n+k_3)) \n
      * y_{n+1}=1/6*k_1+1/3*k_2+1/3*k_3+1/6*k_4 \n
-     * @param M number of time steps
+     * @param M number of steps
      */
-    void solve(int M) override;
+    void solve() override;
 
 };
 #endif //PROJECT_ODE_SYSTEM_H
