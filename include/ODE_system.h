43afa4eccb15fcc2bd57f7b4280510465189331b
GauvainWU <gauvain.wujiahua@gmail.com>
2018-12-08 20:46:37 +0100

    Rename file folders

---
+++ include/ODE_system.h
@@ -0,0 +1,144 @@
+
+
+
+#ifndef PROJECT_ODE_SYSTEM_H
+#define PROJECT_ODE_SYSTEM_H
+
+/// STL include
+#include <vector>
+#include <functional>
+#include <string>
+#include <cassert>
+#include <iostream>
+
+/// local include
+#include "ODE_solver.h"
+using namespace std;
+
+///  Inheritance of class exception: failure of creating an output file
+class output_failure:public exception{
+public:
+    virtual const char* what() const throw(){
+        return "The output file fails to be created.\n";
+    }
+};
+
+/** \brief Define the function pointer type */
+typedef Vector(*Vec_Funpointer)(Real);
+
+///This abstract class stores essential ingredients of a vectorial ode system as well as a solution obtained by using specified solver functions.
+class ODE_System{
+
+public:
+
+    /** \brief Constructor. Initializes ODE_System data structures*/
+    ODE_System(Real t0, Real tn, const Vector& y00, const Matrix& A, Vector g(Real), int M);
+    /** \brief Destructor. Deallocate memory used by std::vector objects*/
+    virtual ~ODE_System();
+
+    /** \brief Accessor to get the solution*/
+    Matrix access_solution() const;
+
+    /**
+     * \brief Pure virtual method. Call the solvers to solve the system.
+     * @param M: number of time steps
+     */
+    virtual void solve() = 0;
+
+    /**
+     * \brief Output the solution as filetype given by the user\n
+     * @param precision: number of significant numbers\n
+     * @param filename: name of the file in form of a string\n
+     * @param delimiter: character that seperates values
+     */
+    void write_solution(string filename,int precision = 5, char delimiter = ',');
+
+protected:
+    /** \brief Initial time*/
+    Real t0;
+    /** \brief End time*/
+    Real tn;
+
+    /** \brief number of steps*/
+    int M;
+
+    /** \brief initial condition*/
+    Vector y00;
+    /** \brief Function pointer storing the function g as defined in the problem*/
+    Vec_Funpointer g;
+    /** \brief Matrix A as defined in the problem*/
+    Matrix A;
+
+    /**
+     * \brief Matrix storing the solution\n
+     * ith row = solution vector at ith time step\n
+     * jth col = solution of the jth component at all time steps
+     */
+    Matrix solution;
+};
+
+/// Inheritance of class ODE_System: Solving the vectorial system by means of forward Euler method
+class ForwardEuler_System : public ODE_System{
+public:
+    /** \brief Constructor. Initialize data sturcture. */
+    ForwardEuler_System(Real t0, Real tn, const Vector& y00, const Matrix& A, Vector g(Real),int M);
+
+    /** \brief Destructor. Deallocate memory used by std::vector objects*/
+    ~ForwardEuler_System () override;
+
+    /** \brief Implement explicit forward euler method and store the solution at all time steps in the attribute solution */
+    /** f(t,y)=A*y+g(t)\n
+     * y_{n+1}=y_n+f(t_n,y_n)*h \n
+     * @param M number of steps
+     */
+    void solve() override;
+};
+
+/// Inheritance of class ODE_System: Solving the vectorial system by means of Adams Bashforth method
+class Adams_Bashforth_System : public ODE_System{
+public:
+    /** \brief Constructor. Initialize data sturcture. */
+    Adams_Bashforth_System(Real t0, Real tn, const Vector& y00, const Matrix& A, Vector g(Real),int step,int M);
+
+    /** \brief Destructor. Deallocate memory used by std::vector objects*/
+    ~Adams_Bashforth_System() override;
+
+    /** \brief Implement Adams Bashforth method and store the solution at all time steps in the attribute solution */
+    /** f(t,y)=A*y+g(t)\n
+     * step=1: Adams Bashforth method is the same as Forward Euler method. \n
+     * step=2: y_{n+2}=y_{n+1}+h*(3/2*f(t_{n+1},y_{n+1})-1/2*f(t_n,y_n)) \n
+     * step=3: y_{n+3}=y_{n+2}+h*(23/12*f(t_{n+2},y_{n+2})-16/12*f(t_{n+1},y_{n+1})+5/12*f(t_n,y_n))\n
+     * step=4: y_{n+4}=y_{n+3}+h*(55/24*f(t_{n+3},y_{n+3})-59/24*f(t_{n+2},y_{n+2})+37/24*f(t_{n+1},y_{n+1})-9/24*f(t_n,y_n)) \n
+     *  @param M number of steps
+     */
+    void solve() override;
+
+
+protected:
+    /** \brief step order of the method */
+    int step;
+};
+
+/// Inheritance of class ODE_System: Solving the vectorial system by means of Runge Kutta 4th order method
+class RKSystem4th_System : public ODE_System{
+public:
+    /** \brief Constructor. Initialize data sturcture.*/
+    RKSystem4th_System(Real t0, Real tn, const Vector& y00,const Matrix& A, Vector g(Real),int M);
+
+    /** \brief Destructor. Deallocate memory used by std::vector objects*/
+    ~RKSystem4th_System() override;
+
+    /** \brief Implement Adams Bashforth method and store the solution at all time steps in the attribute solution */
+    /** f(t,y)=A*y+g(t) \n
+     * k_1=f(t_n,y_n)) \n
+     * k_2=f(t_n+0.5*h,y_n+0.5*k_1)) \n
+     * k_3=f(t_n+0.5*h,y_n+0.5*k_2)) \n
+     * k_4=f(t_n+h,y_n+k_3)) \n
+     * y_{n+1}=1/6*k_1+1/3*k_2+1/3*k_3+1/6*k_4 \n
+     * @param M number of steps
+     */
+    void solve() override;
+
+};
+
+#endif //PROJECT_ODE_SYSTEM_H
---
+++ include/input.h
@@ -0,0 +1,125 @@
+//
+// Created by Jiahua WU on 04.12.18.
+//
+
+#ifndef ODE_SOLVER_INPUT_H
+#define ODE_SOLVER_INPUT_H
+
+#include "ODE_solver.h"
+#include <string>
+using namespace std;
+
+/// Structure storing parameters read from the setting file
+struct Setting{
+    int M;
+    int precision = 5;
+    int usual_function = 1;
+    Real t0;
+    Real tn;
+    string path_y00;
+    string path_A;
+    string path_solution;
+    char delimiter_data = ' ';
+    char delimiter_solution = ',';
+};
+
+/// Usual Function Input module. Return a matrix containing the parameters, the functions choices and the dimension.
+Matrix Usual_Function_Input();
+
+
+///Abstract class of readers
+class Reader{
+
+protected:
+    /// \brief Path of the file to be read
+    string path;
+
+public:
+
+    /// \brief Constructor. Initialize the attribute path with the path given
+    explicit Reader(const string& path);
+
+    /// \brief Pure virtual function for reading in from files.
+    virtual void read_in() = 0;
+};
+
+/// Inheritance of class Reader which specializes in reading data from files
+class Data_Reader : public Reader{
+protected:
+
+    /// \brief delimiter used in the file to be read
+    char delimiter;
+
+public:
+
+    /// \brief Constructor. Initialize the attributes with the input path and delimiter
+    Data_Reader(const string& path,char delimiter);
+    /// \brief Default destructor to be override
+    virtual ~Data_Reader() = default;
+};
+
+/// Inheritance of class Data_Reader which specializes in reading files storing a vector
+class Vector_Reader : public Data_Reader{
+protected:
+    /// \brief Vector read from the file
+    Vector y;
+
+public:
+
+    /// \brief Constructor. Initialize data structure with the input arguments
+    explicit Vector_Reader(const string& path,char delimiter = ' ');
+
+    /// \brief Destructor. Deallocate the memory used by the vector attribute.
+    ~Vector_Reader() override;
+
+    /// \brief Accessor. Access the vector read from the file
+    Vector access_vector() const;
+
+    /// \brief Read the file from the path attribute and assign it to the vector attribute
+    void read_in() override;
+
+};
+
+/// Inheritance of class Data_Reader which specializes in reading files storing a matrix
+class Matrix_Reader : public Data_Reader{
+protected:
+
+    /// \brief Matrix read from the file
+    Matrix A;
+
+public:
+    /// \brief Constructor. Initialize data structure with the input arguments
+    explicit Matrix_Reader(const string& path, char delimiter = ' ');
+
+    /// \brief Destructor. Deallocate the memory used by the Matrix attribute
+    ~Matrix_Reader() override;
+
+    /// \brief Accessor. Access the matrix read from the file
+    Matrix access_matrix() const;
+
+    /// \brief Read the file from the path attribute and assign it to the matrix attribute
+    void read_in() override;
+
+};
+
+/// Inheritance of class Reader specialized in reading the setting file
+class Setting_Reader : public Reader{
+protected:
+    /// \brief Attribute that stores the parameters read from the setting file
+    Setting setting;
+
+public:
+
+    /// \brief Constructor. Initialize the attributes with the input argument
+    explicit Setting_Reader(const string& path);
+
+   /// \brief Accessor. Provide access to the setting parameters
+    Setting access_setting() const;
+
+    /// \brief Read the file from the path attribute and assign it to the setting attribute
+    void read_in() override;
+
+};
+
+
+#endif //ODE_SOLVER_INPUT_H
