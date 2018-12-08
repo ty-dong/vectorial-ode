aa70e22202d6242deca3652fa9a1d001d0d04628
GauvainWU <gauvain.wujiahua@gmail.com>
2018-12-08 19:36:52 +0100

    Rebaptise file folder names

---
+++ settings/user_defined_g.h
@@ -0,0 +1,16 @@
+//
+// Created by Jiahua WU on 08.12.18.
+//
+
+#ifndef ODE_SOLVER_USER_DEFINED_G_H
+#define ODE_SOLVER_USER_DEFINED_G_H
+
+// To import typedef Vector and Matrix 
+#include "../include/ODE_solver.h"
+
+Vector user_define(Real t){
+    Vector v;
+    return v;
+};
+
+#endif //ODE_SOLVER_USER_DEFINED_G_H
